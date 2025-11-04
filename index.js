067eeaf36d2ed69008e462882b34ca7dc120756c
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2025-11-04 09:12:13 +0600

    remaining added

--- index.js
+++ index.js
@@ -121,6 +121,24 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/products/bids/:productId" , async(req,res)=>{
+      const productId = req.params.productId;
+      const query = {product : productId};
+      const cursor = bidsCollection.find(query).sort({bid_price: -1})
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/bids", async(req,res)=>{
+      const query = {}
+      if(query.email){
+        query.buyer_email = email;
+      }
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
     app.post("/bids", async (req, res) => {
       const newBid = req.body;
       const result = await bidsCollection.insertOne(newBid);
@@ -129,7 +147,7 @@ async function run() {
 
     app.delete("/bids/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const result = await bidsCollection.deleteOne(query);
       res.send(result);
     });
